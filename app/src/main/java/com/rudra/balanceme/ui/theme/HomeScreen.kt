6f2100520686dfbc4180d9ada953bc8225159fad
Your Name <your-email@example.com>
2025-04-12 21:06:29 +0600

    up

--- app/src/main/java/com/rudra/balanceme/ui/theme/HomeScreen.kt
+++ app/src/main/java/com/rudra/balanceme/ui/theme/HomeScreen.kt
@@ -6,8 +6,10 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+@Preview
 @Composable
 fun HomeScreen() {
     var mood by remember { mutableStateOf(5f) }
@@ -44,3 +46,5 @@ fun HomeScreen() {
         }
     }
 }
+
+
